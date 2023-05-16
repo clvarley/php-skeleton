393794b71e50976e468cdbf36726e7a1f48a7b79
clvarley <conorlukevarley@gmail.com>
2023-05-16 17:20:23 +0100

    Initial commit

---
+++ src/Example.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Clvarley;
+
+/**
+ * Create your classes in the correct namespaces following PSR-4 style
+ * 
+ * @see https://www.php-fig.org/psr/psr-4/
+ */
+final class Example {}
